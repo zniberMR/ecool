69a8d3ab7644d196100bbc0501c41c0c1bce6a1b
Mehdi Rtn <mehdi.ritouni@gmail.com>
2021-01-10 18:19:27 +0100

    v2

--- src/main/java/company/victoria/ecool/repository/course/ParagraphRepository.java
+++ src/main/java/company/victoria/ecool/repository/course/ParagraphRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
