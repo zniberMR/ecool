d2b1fee2663087def6514f2bee18394b67cf5120
Mehdi Rtn <mehdi.ritouni@gmail.com>
2021-01-10 18:12:42 +0100

    v2

--- src/main/java/company/victoria/ecool/repository/course/ParagraphRepository.java
+++ src/main/java/company/victoria/ecool/repository/course/ParagraphRepository.java
@@ -5,15 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface ParagraphRepository  extends JpaRepository<Paragraph, Long> {
 
+    @Transactional
     @Query("SELECT p FROM Paragraph p where p.article.id = :articleId")
     List<Paragraph> findByArticleId(@Param("articleId") Long articleId);
 
+    @Transactional
     @Query("SELECT p FROM Paragraph p WHERE p.article.section.part.course.isCompleted = true AND p.content LIKE CONCAT ('%', :keyword, '%')")
     List<Paragraph> search(@Param("keyword") String keyword);
 
